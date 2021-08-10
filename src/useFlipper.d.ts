82ec2b3f4d34c599dbe5fcf706c0399f4b3ff29a
Rimuy <kevinntavares@gmail.com>
2021-08-10 20:25:44 -0300

    Fix useFlipper typing

--- src/useFlipper.d.ts
+++ src/useFlipper.d.ts
@@ -2,9 +2,7 @@ import { Binding } from '@rbxts/roact';
 import { CoreHooks } from '@rbxts/roact-hooks';
 import { SingleMotor, GroupMotor } from '@rbxts/flipper';
 
-type FlipperMotor<T = never> = T extends never ? SingleMotor : GroupMotor<T>
-
 export default function useFlipper<T>(
-        motor: FlipperMotor<T>,
+        motor: T,
         hooks: CoreHooks
-): LuaTuple<[Binding<number>, FlipperMotor<T>]>;
+): LuaTuple<[Binding<number>, T]>;
