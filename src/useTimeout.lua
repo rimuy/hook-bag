c3cd9cb809f32adecae7656b096c2c2a2f0bdf1d
Rimuy <kevinntavares@gmail.com>
2021-10-02 23:26:21 -0300

    Update useTimeout

--- src/useTimeout.lua
+++ src/useTimeout.lua
@@ -3,24 +3,36 @@ local Promise = require(script.Parent.Promise)
 local function useTimeout(time, callback, onCancel)
         return function(hooks)
                 local now = hooks.useValue(os.time())
-                local timeout = hooks.useValue(
-                        Promise.delay(time):andThen(function(resolve)
+
+                local createTimeout = hooks.useCallback(function()
+                        return Promise.delay(time):andThen(function(resolve)
                                 task.spawn(function()
                                         callback()
                                 end)
                                 resolve()
-                        end):catch(function() end))
-
-                local cancel = hooks.useCallback(function()
-                        if timeout.value.Status == "Started" then
-                                timeout.value:cancel()
-                                if onCancel then
-                                        onCancel(os.time() - now)
-                                end
-                        end
+                        end):catch(function() end)
                 end, {})
 
-                return cancel
+                local timeout = hooks.useValue(createTimeout())
+
+                local cancel = hooks.useCallback(function()
+                        if timeout.value:getStatus() == "Started" then
+                                timeout.value:cancel()
+                                if onCancel then
+                                        onCancel(os.time() - now.value)
+                                end
+                        end
+                end, { timeout.value })
+
+                local reset = hooks.useCallback(function()
+                        cancel()
+                        timeout.value = createTimeout()
+                end, {})
+
+                return {
+                        cancel = cancel,
+                        reset = reset,
+                }
         end
 end
 
