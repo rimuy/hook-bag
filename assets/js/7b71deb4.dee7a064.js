d1504f640dfccf338df31a3ef0f526cff6d8dda7
Rimuy <kevinntavares@gmail.com>
2021-10-10 06:06:19 -0300

    Built and published by Moonwave

---
+++ assets/js/7b71deb4.dee7a064.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[475],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/HylianBasement/rbx-hook-bag/blob/master","baseUrl":"/rbx-hook-bag/","classOrder":["Hooks"]}')}}]);
--- assets/js/7b71deb4.f8afb812.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[475],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/HylianBasement/rbx-hook-bag/blob/master","baseUrl":"/","classOrder":["Hooks"]}')}}]);
